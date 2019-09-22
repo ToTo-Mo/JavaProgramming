cb0efffdc493f20271a485ba98d20856dfb83be4
ToTo-Mo <46367491+ToTo-Mo@users.noreply.github.com>
2019-09-22 13:21:12 +0900

    Title : 2장 생성자 소멸자 과제 완료

--- src/Example/Date.java
+++ src/Example/Date.java
@@ -6,60 +6,13 @@ public class Date
     private int month;
     private int day;
 
-    public int year()
-    {
-        return year;
-    }
-
-    public void year(int value)
-    {
-        year =value;
-    }
-
-    public int month()
-    {
-        return month;
-    }
-
-    public void month(int value)
-    {
-        month = value;
-    }
-
-    public int day()
-    {
-        return day();
-    }
-
-    public void day(int value)
-    {
-        day = value;
-    }
-
-    public void print()
-    {
-        System.out.println(year + "년 " + month+"월 "+day+"일");
-    }
-
-    public static void program()
-    {
-        Date date = new Date();
-        date.year(2010);
-        date.month(1);
-        date.day(20);
-
-        date.print();
-    }
-
-    public Date(int y,int m,int d)
-    {
+    public Date(int y,int m,int d){
         year =y;
         month =m;
         day =d;
     }
 
-    public Date(int y)
-    {
+    public Date(int y){
         this(y,1,1);
     }
 
@@ -67,4 +20,41 @@ public class Date
         this(2010,1,1);
     }
 
+    public int year(){
+        return year;
+    }
+
+    public void year(int value){
+        year =value;
+    }
+
+    public int month(){
+        return month;
+    }
+
+    public void month(int value){
+        month = value;
+    }
+
+    public int day(){
+        return day();
+    }
+
+    public void day(int value){
+        day = value;
+    }
+
+    public void print(){
+        System.out.println(year + "년 " + month+"월 "+day+"일");
+    }
+
+    public static void program(){
+        Date date1 = new Date(2009,3,2);
+        Date date2 = new Date(2009);
+        Date date3 = new Date();
+
+        date1.print();
+        date2.print();
+        date3.print();
+    }
 }
--- src/MainProgram/Program.java
+++ src/MainProgram/Program.java
@@ -1,9 +1,9 @@
 package MainProgram;
 import Example.*;
-import Syntax.*;
-import Syntax.Array.ArrayDeclaration;
+
 public class Program {
 	public static void main(String[] args) {
+		Time.program();
 	}
 }
 
